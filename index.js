7603b12209121fbd7e4a1331b127cd6d406c882b
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-03 11:31:57 +0900

    refactor formatting and improve SQL query in /view endpoint

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const mysql = require("mysql2");
 const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // html form -> object
+app.use(express.json()); //json->object
+app.use(express.urlencoded({ extended: true })); //html form ->object
 
 let corsOptions = {
   origin: "*",
@@ -37,11 +37,26 @@ app.get("/list", (req, res) => {
   });
 });
 
+app.get("/view", (req, res) => {
+  console.log(req.query.id);
+
+  const id = req.query.id;
+  // const sqlQuery = `SELECT * FROM board WHERE id=${req.query.id};`;
+  const sqlQuery =
+    "SELECT title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=?;";
+
+  db.query(sqlQuery, [id], (err, result) => {
+    if (err) throw err;
+
+    res.send(result);
+  });
+});
+
 app.post("/write", (req, res) => {
   console.log(req.body);
 
   const { title, name, content } = req.body;
-  const sqlQuery = " insert into board (title,content,writer) values (?,?,?);";
+  const sqlQuery = "insert into board (title, content, writer) values (?, ?, ?);";
 
   db.query(sqlQuery, [title, content, name], (err, result) => {
     if (err) throw err;
